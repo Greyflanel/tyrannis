1a2d7da65aec55c5a93b90a11bb85462f89f4eca
Greyflanel <richardlovelace811@gmail.com>
2019-09-24 14:56:55 -0500

    Created an array, filtered it by name array items, passed props into component and rendered to display.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import Jinxy from './Jinxy'
 
-const App = () => {
-    return <div>APP</div>
+class App extends React.Component {
+    
+state = {
+    colors: ['red', 'green', 'blue']
+};
+
+componentDidMount() {
+  this.setState  ({
+  colors: [...this.state.colors, "yellow", "purple", "orange"]
+});
 }
+  render() {
+      console.log(this.state.colors);
+
+    return (
+      <div className="container">
+    <div className="color">
+    <Jinxy colors={this.state.colors.filter(color => 
+      color !== "red"
+    ).join(' ')}/>
+    </div>
+    </div>
+     )};
+    };
 
 export default App;
---
+++ src/components/Jinxy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Jinxy = props => {
+    return (
+    <div>
+        JINXY
+<p>{props.colors}</p>
+    </div>
+    );
+};
+
+export default Jinxy;
